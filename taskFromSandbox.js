2d90ac68ee585bd20e71dcf3ce329299a3e90f63
Rumata <kirr7619@gmail.com>
2022-02-05 16:32:22 +0500

    Added task № 2 7 kyu

--- taskFromSandbox.js
+++ taskFromSandbox.js
@@ -1,5 +1,5 @@
 "use strict";
-// task № 1 8 kyu---------------------------
+// Task № 1 8 kyu---------------------------
 /* 
       Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
@@ -13,5 +13,25 @@ function digitize(n) {
             n[i] = +n[i];
       }
       return n;
+}
+ */
+
+// Task № 2 7 kyu---------------------------
+/*
+      Implement a function that accepts 3 integer values a, b, c.
+      The function should return true if a triangle can be built 
+      with the sides of given length and false in any other case. 
+ */
+/*
+      Реализуйте функцию, которая принимает 3 целочисленных значения a, b, c. Функция должна возвращать true, если можно построить треугольник со сторонами заданной длины, и false в любом другом случае. 
+ */
+/*
+function isTriangle(a, b, c) {
+      let arr = [a, b, c];
+      arr = arr.sort((a, b) => a - b);
+      if (arr[0] + arr[1] > arr[2]) {
+            return true;
+      }
+      return false;
 }
  */
