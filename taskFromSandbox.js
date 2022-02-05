d07bc6f002efde252b717eca6113cbcd2a291b0c
Rumata <kirr7619@gmail.com>
2022-02-05 18:40:40 +0500

    Added Task № 8 8 kyu "Beginner - Lost Without a Map"

--- taskFromSandbox.js
+++ taskFromSandbox.js
@@ -145,3 +145,22 @@ function greet(name) {
       return `Hello, ${name} how are you doing today?`;
 }
  */
+
+// Task № 8 8 kyu---------------------------
+// Beginner - Lost Without a Map
+/*
+      Given an array of integers, return a
+      new array with each value doubled. 
+ */
+/* 
+      Учитывая массив целых чисел, вернуть новый
+      массив с удвоением каждого значения.
+*/
+/* 
+function maps(x) {
+      let a = [];
+      for (let i = 0; i < x.length; i++) {
+            a.push(x[i] * 2);
+      }
+      return a;
+} */
