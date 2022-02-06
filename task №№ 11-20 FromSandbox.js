9135a8a4415ae50f8314637d7be85af02a54bffa
Rumata <kirr7619@gmail.com>
2022-02-06 13:40:44 +0500

    Added Task № 15 8 kyu "Calculate average"

--- task №№ 11-20 FromSandbox.js
+++ task №№ 11-20 FromSandbox.js
@@ -93,3 +93,28 @@ function basicOp(operation, value1, value2) {
       }
 }
  */
+
+// Task № 15 8 kyu---------------------------
+// Calculate average
+/*
+      Write a function which calculates the average
+      of the numbers in a given list.
+      Note: Empty arrays should return 0. 
+*/
+/*
+      Напишите функцию, которая вычисляет среднее
+      значение чисел в заданном списке.
+      Примечание. Пустые массивы должны возвращать 0.
+*/
+/* 
+function findAverage(array) {
+      let sum = 0;
+      if (array.length) {
+            for (let i = 0; i < array.length; i++) {
+                  sum += array[i];
+            }
+            return sum / array.length;
+      }
+      return 0;
+}
+ */
