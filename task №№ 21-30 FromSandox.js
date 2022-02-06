ad92ba26d1ba0493f67ada80f1bfc13f84d092fb
Rumata <kirr7619@gmail.com>
2022-02-06 20:44:19 +0500

    Added Task № 24 8 kyu "Sum of positive"

--- task №№ 21-30 FromSandox.js
+++ task №№ 21-30 FromSandox.js
@@ -74,3 +74,34 @@ function filterList(l) {
       return arr;
 }
  */
+
+// Task № 24 8 kyu---------------------------
+// Sum of positive
+/*
+      You get an array of numbers, return the sum
+      of all of the positives ones.
+      Example [1,-4,7,12] => 1 + 7 + 12 = 20
+      Note: if there is nothing to sum, the sum is
+      default to 0. 
+ */
+/*
+      Вы получаете массив чисел, возвращаете сумму 
+      всех положительных.
+      Пример [1,-4,7,12]=>1 + 7 + 12 = 20
+      Примечание: если суммировать нечего,
+      сумма по умолчанию равна 0. 
+ */
+/* 
+function positiveSum(arr) {
+      if (arr.length == 0) {
+            return 0;
+      }
+      let sum = 0;
+      arr.forEach((element) => {
+            if (element > 0) {
+                  sum += element;
+            }
+      });
+      return sum;
+}
+ */
