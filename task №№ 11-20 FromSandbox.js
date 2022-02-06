b6c88aa440aa7c290d97f27edef579919d0887c6
Rumata <kirr7619@gmail.com>
2022-02-06 13:00:36 +0500

    Added Task № 14 8 kyu "Basic Mathematical Operations"

--- task №№ 11-20 FromSandbox.js
+++ task №№ 11-20 FromSandbox.js
@@ -60,3 +60,36 @@ function even_or_odd(number) {
       return number % 2 ? "Odd" : "Even";
 }
  */
+// Task № 14 8 kyu---------------------------
+// Basic Mathematical Operations
+/*
+      Your task is to create a function that does four
+      basic mathematical operations.
+      The function should take three arguments -
+      operation(string/char), value1(number),
+      value2(number).
+      The function should return result of numbers 
+      after applying the chosen operation. 
+*/
+/*
+      Ваша задача — создать функцию, которая выполняет
+      четыре основные математические операции.
+      Функция должна принимать три аргумента - 
+      операция(строка/символ), значение1(число),
+      значение2(число).
+      Функция должна возвращать числовой результат
+      после применения выбранной операции. 
+*/
+/* 
+function basicOp(operation, value1, value2) {
+      if(operation === "+"){
+            return value1 + value2;
+      }else if(operation === "-"){
+            return value1 - value2;
+      }else if(operation === "*"){
+            return value1 * value2;
+      }else if(operation === "/"){
+            return value1 / value2;
+      }
+}
+ */
