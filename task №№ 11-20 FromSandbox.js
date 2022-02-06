97da1dfe5b70004ad2ae0a4fa3039e9f997c0461
Rumata <kirr7619@gmail.com>
2022-02-06 12:28:36 +0500

    Added Task № 11 8 kyu
    "Convert boolean values to strings 'Yes' or 'No'"

--- task №№ 11-20 FromSandbox.js
+++ task №№ 11-20 FromSandbox.js
@@ -1,3 +1,18 @@
 "use strict";
-// Task № 11  kyu---------------------------
-//
+// Task № 11 8 kyu---------------------------
+// Convert boolean values to strings 'Yes' or 'No'.
+/*
+      Complete the method that takes a boolean value
+      and return a "Yes" string for true,
+      or a "No" string for false.  
+*/
+/*
+      Завершите метод, который принимает логическое значение
+      и возвращает "Yes"строку для true,
+      или "No"строку для false. 
+*/
+/* 
+function boolToWord(bool) {
+      return bool ? "Yes" : "No";
+}
+ */
