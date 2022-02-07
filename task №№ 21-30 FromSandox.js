48175138b1a4b3b4b7dd9af709e4b2800597205f
Rumata <kirr7619@gmail.com>
2022-02-07 11:22:00 +0500

    Added Task № 26 8 kyu "Will you make it?"

--- task №№ 21-30 FromSandox.js
+++ task №№ 21-30 FromSandox.js
@@ -139,3 +139,34 @@ function booleanToString(b) {
       return b + "";
 }
  */
+
+// Task № 26 8 kyu---------------------------
+// Will you make it?
+/*
+      You were camping with your friends far away from 
+      home, but when it's time to go back, you realize
+      that your fuel is running out and the nearest 
+      pump is 50 miles away! You know that on average
+      your car runs on about 25 miles per gallon.
+      There are 2 gallons left. Considering these
+      factors, write a function that tells you
+      if it is possible to get to the pump or not. Function should return true (1 in Prolog
+      and NASM) if it is possible and
+      false (0 in Prolog and NASM) if not.
+      The input values are always positive. 
+*/
+/*
+      Вы были в походе с друзьями далеко от дома,
+      но когда пришло время возвращаться, вы понимаете,
+      что топливо на исходе, а ближайшая заправка
+      50далеко! Вы знаете, что в среднем ваш автомобиль расходует около 25миль на галлон. Остаются
+      2галлоны. Учитывая эти факторы, напишите функцию, которая говорит вам, можно ли добраться до
+      насоса или нет. Функция должна возвращать true
+      ( 1в Прологе и NASM), если это возможно, 
+      и false( 0в Прологе и NASM), если нет. Входные значения всегда положительны. 
+*/
+/* 
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+      return fuelLeft * mpg >= distanceToPump;
+};
+ */
