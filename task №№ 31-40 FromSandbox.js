4d6ebcbc0f8f0e038d050810ee6ac09b6180b260
Rumata <kirr7619@gmail.com>
2022-02-07 12:12:06 +0500

    Added Task № 31 7 kyu "Isograms"

--- task №№ 31-40 FromSandbox.js
+++ task №№ 31-40 FromSandbox.js
@@ -1,3 +1,36 @@
 "use strict";
 
 // Task № 31 7 kyu---------------------------
+// Isograms
+/*
+      An isogram is a word that has no repeating letters,
+      consecutive or non-consecutive. Implement a 
+      function that determines whether a string that 
+      contains only letters is an isogram. Assume the 
+      empty string is an isogram. Ignore letter case.
+      Example: (Input --> Output)
+      "Dermatoglyphics" --> true
+      "aba" --> false
+ */
+/*
+      Изограмма — это слово, в котором нет повторяющихся 
+      букв, последовательных или непоследовательных.
+      Реализуйте функцию, определяющую, является ли строка, содержащая только буквы, изограммой. Предположим,
+      что пустая строка является изограммой. Игнорировать
+      регистр букв.
+      Пример: (Ввод --> Вывод)
+      "Dermatoglyphics" --> true
+      "aba" --> false
+*/
+/* 
+function isIsogram(str) {
+      str = str.toLowerCase();
+      for (let i = 0; i < str.length; ++i) {
+            for (let j = i + 1; j < str.length; ++j) {
+                  if (str[i] === str[j]) {
+                        return false;
+                  }
+            }
+      }
+      return true;
+} */
