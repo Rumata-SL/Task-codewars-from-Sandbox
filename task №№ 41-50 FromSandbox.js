721b4f5bc60aa79a5a7aa12f5ffaa1a8471e7501
Rumata <kirr7619@gmail.com>
2022-02-07 21:43:29 +0500

    Added Task № 46 8 kyu "Sum Mixed Array"

--- task №№ 41-50 FromSandbox.js
+++ task №№ 41-50 FromSandbox.js
@@ -127,3 +127,23 @@ function setAlarm(employed, vacation) {
       return employed && !vacation ? true : false;
 }
  */
+
+// Task № 46 8 kyu---------------------------
+// Sum Mixed Array
+/*
+      Given an array of integers as strings and numbers,
+      return the sum of the array values as if all were
+      numbers.
+      Return your answer as a number. 
+*/
+/*
+      Учитывая массив целых чисел в виде строк и чисел,
+      верните сумму значений массива, как если бы
+      все они были числами.
+      Верните ответ в виде числа. 
+ */
+/* 
+function sumMix(x) {
+      return x.reduce((x, y) => Number(x) + Number(y), 0);
+}
+ */
