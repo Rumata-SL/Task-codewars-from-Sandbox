48d5a97cbe30f80c1a207a25fbb344863a2aa9ea
Rumata <kirr7619@gmail.com>
2022-02-07 21:55:03 +0500

    Added Task № 48 8 kyu "Convert a Number to a String!"

--- task №№ 41-50 FromSandbox.js
+++ task №№ 41-50 FromSandbox.js
@@ -174,3 +174,26 @@ function sumMix(x) {
             .split(".")[0];
 }
  */
+// Task № 48 8 kyu---------------------------
+// Convert a Number to a String!
+/*
+      We need a function that can transform a
+      number into a string.
+      What ways of achieving this do you know?
+      Examples:
+      123 --> "123"
+      999 --> "999" 
+*/
+/*
+      Нам нужна функция, которая может преобразовать
+      число в строку.
+      Какие способы достижения этого вы знаете?
+      Примеры:
+      123 --> "123"
+      999 --> "999"
+*/
+/* 
+function numberToString(num) {
+      return num + "";
+}
+ */
