659f1dd8a18309713ff386ed31829486a9bd41c0
Rumata <kirr7619@gmail.com>
2022-02-07 20:49:55 +0500

    Added Task № 41 8 kyu "Sum Arrays"

--- task №№ 41-50 FromSandbox.js
+++ task №№ 41-50 FromSandbox.js
@@ -1,1 +1,34 @@
 "use strict";
+// Task № 41 8 kyu---------------------------
+// Sum Arrays
+/*
+      Write a function that takes an array of numbers 
+      and returns the sum of the numbers. The numbers
+      can be negative or non-integer. If the array does
+      not 
+      contain any numbers then you should return 0.
+      Examples
+      Input: [1, 5.2, 4, 0, -1]
+      Output: 9.2 
+*/
+/* 
+      Напишите функцию, которая принимает массив чисел 
+      и возвращает сумму чисел. Числа могут быть 
+      отрицательными или нецелыми. Если массив не 
+      содержит чисел, вы должны вернуть 0.
+      Примеры
+      Вход: [1, 5.2, 4, 0, -1]
+      Выход:9.2
+ */
+/* 
+function sum(numbers) {
+      let sum = 0;
+      if (numbers.length === 0) {
+            return 0;
+      }
+      numbers.forEach((element) => {
+            sum += element;
+      });
+      return sum;
+} 
+*/
