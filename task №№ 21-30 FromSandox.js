4988b9b78ee1d55a7b9710c87f582e964189ebb3
Rumata <kirr7619@gmail.com>
2022-02-07 11:54:38 +0500

    Added Task № 30 7 kyu "Reverse words"

--- task №№ 21-30 FromSandox.js
+++ task №№ 21-30 FromSandox.js
@@ -217,3 +217,33 @@ function solution(string) {
       return string.split(/(?=[A-Z])/).join(" ");
 }
  */
+
+// Task № 30 7 kyu---------------------------
+// Reverse words
+/*
+      Complete the function that accepts a string
+      parameter, and reverses each word in the string.
+      All spaces in the string should be retained.
+      Examples
+      "This is an example!" ==> "sihT si na !elpmaxe"
+      "double  spaces"      ==> "elbuod  secaps" 
+*/
+/*
+      Завершите функцию, которая принимает строковый 
+      параметр и переворачивает каждое слово в строке.
+      Все пробелы в строке должны быть сохранены.
+      Примеры
+      "This is an example!" ==> "sihT si na !elpmaxe"
+      "double  spaces"      ==> "elbuod  secaps" 
+*/
+/* 
+function reverseWords(str) {
+      str = str.split([" "]);
+
+      for (let i = 0; i < str.length; i++) {
+            str[i] = str[i].split("").reverse();
+            str[i] = str[i].join("");
+      }
+      str = str.join(" ");
+      return str;
+} */
