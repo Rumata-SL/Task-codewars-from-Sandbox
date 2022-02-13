de14e9a0b919dff1eb51a86020c064dd858a67a9
Rumata-SL <kirr7619@gmail.com>
2022-02-14 01:29:40 +0500

    Added task №52 "Square Every Digit"

--- task №№ 51-60 FromSandbox .js
+++ task №№ 51-60 FromSandbox .js
@@ -13,3 +13,15 @@
 /* function friend(friends) {
       return friends.filter((item) => item.length === 4);
 } */
+
+// Task № 52 7 kyu---------------------------
+// Square Every Digit
+/* 
+      Welcome. In this kata, you are asked to square every
+      digit of a number and concatenate them.
+      For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+      Note: The function accepts an integer and returns an integer
+ */
+/* function squareDigits(num) {
+      return +[...(num + "")].map((el) => el * el).join``;
+} */
