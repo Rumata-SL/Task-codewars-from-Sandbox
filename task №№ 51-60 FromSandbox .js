d30861205088ebe79a7cae356701d85436dd2f6a
Rumata-SL <kirr7619@gmail.com>
2022-02-14 01:45:28 +0500

    Added task №53 "Beginner - Reduce but Grow"

--- task №№ 51-60 FromSandbox .js
+++ task №№ 51-60 FromSandbox .js
@@ -17,11 +17,27 @@
 // Task № 52 7 kyu---------------------------
 // Square Every Digit
 /* 
-      Welcome. In this kata, you are asked to square every
-      digit of a number and concatenate them.
-      For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
-      Note: The function accepts an integer and returns an integer
- */
+Welcome. In this kata, you are asked to square every
+digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+Note: The function accepts an integer and returns an integer
+*/
 /* function squareDigits(num) {
       return +[...(num + "")].map((el) => el * el).join``;
+} */
+
+// Task № 53 8 kyu---------------------------
+// Beginner - Reduce but Grow
+/*
+      Given a non-empty array of integers, 
+      return the result of multiplying the 
+      values together in order. Example:
+      [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 
+ */
+/* function grow(x) {
+      let sum = 1;
+      for (let i = 0; i < x.length; i++) {
+            sum *= x[i];
+      }
+      return sum;
 } */
