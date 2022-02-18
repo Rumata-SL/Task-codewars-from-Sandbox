957601d38ed1258a56a6881149373850e229d17b
Rumata-SL <kirr7619@gmail.com>
2022-02-18 15:58:44 +0500

    Added task № 57 8 kyu "You only need one - Beginner"

--- task №№ 51-60 FromSandbox .js
+++ task №№ 51-60 FromSandbox .js
@@ -136,3 +136,23 @@ function validatePIN(pin) {
             : false;
 }
  */
+
+// Task № 57 8 kyu---------------------------
+// You only need one - Beginner
+/*
+      You will be given an array a and a value x.
+      All you need to do is check whether the provided array 
+      contains the value.
+      Array can contain numbers or strings. X can be either.
+      Return true if the array contains the value, false if not.
+*/
+/*
+      Вам будет предоставлен массив aи значение x.
+      Все, что вам нужно сделать, это проверить,
+      содержит ли предоставленный массив значение.
+      Массив может содержать числа или строки. Х может быть любым.
+      Возврат true, если массив содержит значение, falseесли нет. 
+*/
+/* function check(a, x) {
+      return a.includes(x) ? true : false;
+} */
