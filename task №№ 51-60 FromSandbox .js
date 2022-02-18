9b3378afcd1e55bc1ad5f70053bd2c1b811cac39
Rumata-SL <kirr7619@gmail.com>
2022-02-18 15:09:26 +0500

    Added task №54 "Rot13"

--- task №№ 51-60 FromSandbox .js
+++ task №№ 51-60 FromSandbox .js
@@ -41,3 +41,29 @@ Note: The function accepts an integer and returns an integer
       }
       return sum;
 } */
+// Task № 54 5 kyu---------------------------
+// Rot13
+/*
+      ROT13 is a simple letter substitution cipher that replaces 
+      a letter with the letter 13 letters after it in the alphabet.
+      ROT13 is an example of the Caesar cipher.
+      Create a function that takes a string and returns the string 
+      ciphered with Rot13. If there are numbers or special characters
+      included in the string, they should be returned as they are.
+      Only letters from the latin/english alphabet should be shifted,
+      like in the original Rot13 "implementation". 
+ */
+/* 
+      ROT13 — это простой шифр с заменой букв, который заменяет букву 
+      буквой через 13 букв после нее в алфавите. ROT13
+      является примером шифра Цезаря.
+      Создайте функцию, которая принимает строку и возвращает строку,
+      зашифрованную с помощью Rot13. Если в строку включены числа
+      или специальные символы, они должны быть возвращены как есть.
+      Сдвинуты должны быть только буквы латинского/английского алфавита,
+      как в оригинальной "реализации" Rot13.
+ */
+/* 
+function rot13(message) {
+      return message.replace(/[a-z]/gi, str => String.fromCharCode(str.charCodeAt(0) + (str.toLowerCase() <= 'm' ? 13 : -13)));
+} */
