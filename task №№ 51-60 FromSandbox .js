6b12494a88c44d221ee3d305e01cae330389179f
Rumata-SL <kirr7619@gmail.com>
2022-02-18 15:20:39 +0500

    Added task №55 8 kyu "I love you, a little .."

--- task №№ 51-60 FromSandbox .js
+++ task №№ 51-60 FromSandbox .js
@@ -67,3 +67,49 @@ Note: The function accepts an integer and returns an integer
 function rot13(message) {
       return message.replace(/[a-z]/gi, str => String.fromCharCode(str.charCodeAt(0) + (str.toLowerCase() <= 'm' ? 13 : -13)));
 } */
+
+// Task № 55 8 kyu---------------------------
+// I love you, a little , a lot, passionately ... not at all
+/* 
+      Who remembers back to their time in the schoolyard,
+      when girls would take a flower and tear its petals,
+      saying each of the following phrases each time a petal was torn:
+      I love you
+      a little
+      a lot
+      passionately
+      madly
+      not at all
+      When the last petal was torn there were cries of excitement,
+      dreams, surging thoughts and emotions.
+      Your goal in this kata is to determine which phrase the girls
+      would say for a flower of a given number of petals, where nb_petals > 0.
+ */
+/* 
+      Кто помнит свое время на школьном дворе, когда девочки брали 
+      цветок и рвали его лепестки, произнося каждую из следующих
+      фраз каждый раз, когда отрывался лепесток:
+      я тебя люблю
+      немного
+      много
+      страстно
+      безумно
+      нисколько
+      Когда был сорван последний лепесток, раздались крики волнения,
+      мечты, нахлынувшие мысли и эмоции
+      Ваша цель в этом ката — определить, какую фразу девушки 
+      сказали бы о цветке с заданным количеством лепестков,
+      где nb_petals > 0.
+ */
+/* 
+function howMuchILoveYou(nbPetals) {
+      const str = [
+            "I love you",
+            "a little",
+            "a lot",
+            "passionately",
+            "madly",
+            "not at all",
+      ];
+      return str[(nbPetals - 1) % str.length];
+} */
