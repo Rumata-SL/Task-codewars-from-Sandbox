d8ee3f911b3f3a732b7b88cda5cad859c541b09f
Rumata-SL <kirr7619@gmail.com>
2022-02-20 12:16:33 +0500

    Added task № 61 8 kyu "Function 1 - hello world"

---
+++ task №№ 61-70 FromSandbox.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Task № 61 8 kyu---------------------------
+// Function 1 - hello world
+/*
+      Make a simple function called greet that
+      returns the most-famous "hello world!".
+ */
+/*
+      Создайте простую функцию с именем приветствие ,
+      которая возвращает самое известное «привет, мир!». 
+*/
+/* 
+const greet = () => "hello world!";
+ */
