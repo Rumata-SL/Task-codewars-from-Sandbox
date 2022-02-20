8bda0505eb0212ea9e43c2ba3a34e3dadd44ae0c
Rumata-SL <kirr7619@gmail.com>
2022-02-20 12:41:33 +0500

    Added task № 62 8 kyu "Highest and Lowest"

--- task №№ 61-70 FromSandbox.js
+++ task №№ 61-70 FromSandbox.js
@@ -13,3 +13,20 @@
 /* 
 const greet = () => "hello world!";
  */
+
+// Task № 62 8 kyu---------------------------
+// Highest and Lowest
+/*
+      In this little assignment you are given a string of
+      space separated numbers, and have to return the 
+      highest and lowest number. 
+ */
+/*
+      В этом небольшом задании вам дается строка чисел,
+      разделенных пробелами, и вы должны вернуть
+      наибольшее и наименьшее число. 
+ */
+function highAndLow(numbers) {
+      numbers = numbers.split(" ");
+      return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
