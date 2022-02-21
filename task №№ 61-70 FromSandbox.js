7b91d400ff1cf1886af413cbd613b0364e289cb3
Rumata-SL <kirr7619@gmail.com>
2022-02-21 14:23:19 +0500

    Added task № 67 7 kyu "Vowel Count"

--- task №№ 61-70 FromSandbox.js
+++ task №№ 61-70 FromSandbox.js
@@ -150,3 +150,22 @@ function doubleInteger(i) {
       return (i *= 2);
 }
  */
+
+// Task № 67 7 kyu---------------------------
+// Vowel Count
+/*
+      Return the number (count) of vowels in the given string.
+      We will consider a, e, i, o, u as vowels for this Kata (but not y).
+      The input string will only consist of lower case letters and/or spaces.
+ */
+/*
+      Возвращает количество (количество) гласных в заданной строке.
+      Мы будем рассматривать a, e, i, o, uкак гласные для этой Ката (но не y).
+      Входная строка будет состоять только из строчных букв и/или пробелов.
+*/
+/* 
+function getVowels(str) {
+      str = str.match(/[aeiou]/gi);
+      return str === null ? 0 : str.length;
+}
+ */
