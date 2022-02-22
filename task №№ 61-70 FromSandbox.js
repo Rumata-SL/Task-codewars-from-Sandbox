5fd8080810cefe3d508a22fb3f2adb299ccb193b
Rumata-SL <kirr7619@gmail.com>
2022-02-22 17:52:56 +0500

    Added task № 69 7 kyu "Disemvowel Trolls"

--- task №№ 61-70 FromSandbox.js
+++ task №№ 61-70 FromSandbox.js
@@ -193,3 +193,21 @@ function arithmetic(a, b, operator) {
       return calc[operator];
 }
  */
+
+// Task № 69 7 kyu---------------------------
+// Disemvowel Trolls
+/*
+      Trolls are attacking your comment section!
+      A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+      Your task is to write a function that takes a string and return a new string with all vowels removed.
+      For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!". 
+*/
+/*
+      Тролли атакуют ваш раздел комментариев!
+      Обычный способ справиться с этой ситуацией — удалить все гласные из комментариев троллей, нейтрализуя угрозу.
+      Ваша задача — написать функцию, которая принимает строку и возвращает новую строку, в которой удалены все гласные.
+      Например, строка «Этот сайт для неудачников, LOL!» станет «Ths wbst s fr lsrs LL!». 
+*/
+function disemvowel(str) {
+      return str.replace(/[aeiou]/gi, "");
+}
