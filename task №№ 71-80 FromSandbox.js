a5006b8f9a2dc8135dfc8080e95e021d5af0d772
Rumata-SL <kirr7619@gmail.com>
2022-02-22 18:30:08 +0500

    Added Task № 73 8 kyu "Sum The Strings"

--- task №№ 71-80 FromSandbox.js
+++ task №№ 71-80 FromSandbox.js
@@ -42,3 +42,30 @@ const areaOrPerimeter = function (l, w) {
       return l === w ? l * w : l * 2 + w * 2;
 };
  */
+
+// Task № 73 8 kyu---------------------------
+// Sum The Strings
+/*
+      Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+      Example: (Input1, Input2 -->Output)
+      "4",  "5" --> "9"
+      "34", "5" --> "39"
+      "", "" --> "0"
+      "2", "" --> "2"
+      "-5", "3" --> "-2" 
+*/
+/*
+      Создайте функцию, которая принимает 2 целых числа в виде строки в качестве входных данных и выводит сумму (также в виде строки):
+      Пример: ( Ввод1, Ввод2 -->Вывод )
+      "4",  "5" --> "9"
+      "34", "5" --> "39"
+      "", "" --> "0"
+      "2", "" --> "2"
+      "-5", "3" --> "-2" 
+*/
+/* 
+function sumStr(a, b) {
+      let sum = +a + +b;
+      return sum + "";
+}
+ */
