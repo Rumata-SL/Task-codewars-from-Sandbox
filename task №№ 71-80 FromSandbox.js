d2a710d28690b5345e72fa5e2eec684f045688b4
Rumata-SL <kirr7619@gmail.com>
2022-02-22 18:14:24 +0500

    Added task № 71 8 kyu "Find the smallest integer in the array"

---
+++ task №№ 71-80 FromSandbox.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Task № 71 8 kyu---------------------------
+// Find the smallest integer in the array
+
+/*
+      Given an array of integers your solution should find the smallest integer.
+      For example:
+      Given [34, 15, 88, 2] your solution will return 2
+      Given [34, -345, -1, 100] your solution will return -345 
+ */
+/*
+      Учитывая массив целых чисел, ваше решение должно найти наименьшее целое число.
+      Например:
+      Учитывая [34, 15, 88, 2], что ваше решение вернется2
+      Учитывая [34, -345, -1, 100], что ваше решение вернется-345
+*/
+/* 
+class SmallestIntegerFinder {
+      findSmallestInt(args) {
+            return Math.min(...args);
+      }
+}
+ */
