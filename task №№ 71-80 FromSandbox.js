debc3da3973071c283e1529e03659ec7b399a61e
Rumata-SL <kirr7619@gmail.com>
2022-02-22 21:11:20 +0500

    Added Task № 76 6 kyu "Your order, please"

--- task №№ 71-80 FromSandbox.js
+++ task №№ 71-80 FromSandbox.js
@@ -102,3 +102,37 @@ function removeExclamationMarks(s) {
       return s.replace(/!/g, "");
 }
  */
+
+// Task № 76 6 kyu---------------------------
+// Your order, please
+/*
+      Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+      Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+      If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+      Examples
+      "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+      "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+      ""  -->  "" 
+*/
+/* 
+      Ваша задача — отсортировать заданную строку. Каждое слово в строке будет содержать одно число. Это число и есть позиция, которую должно занимать слово в результате.
+      Примечание. Цифры могут быть от 1 до 9. Таким образом, первым словом будет 1 (а не 0).
+      Если входная строка пуста, вернуть пустую строку. Слова во входной строке будут содержать только допустимые последовательные числа.
+      Примеры
+      "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+      "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+      ""  -->  "" 
+*/
+function order(words) {
+      return words.length === 0
+            ? ""
+            : words
+                    .split(" ")
+                    .sort((pref, next) => {
+                          return (
+                                Number(pref.match(/\d/)) -
+                                Number(next.match(/\d/))
+                          );
+                    })
+                    .join(" ");
+}
