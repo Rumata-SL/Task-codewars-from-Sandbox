bf2ebb417b56209bad7bf7685950568c5608c013
Rumata-SL <kirr7619@gmail.com>
2022-02-23 14:27:11 +0500

    Added Task № 83 8 kyu "Is it even?"

--- task №№ 81-90 FromSandbox.js
+++ task №№ 81-90 FromSandbox.js
@@ -36,3 +36,26 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
       return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
  */
+
+// Task № 83 8 kyu---------------------------
+// Is it even?
+/*
+      In this Kata we are passing a number (n) into a function.
+      Your code will determine if the number passed is even (or not).
+      The function needs to return either a true or false.
+      Numbers may be positive or negative, integers or floats.
+      Floats are considered UNeven for this kata. 
+*/
+/*
+      В этом Ката мы передаем число (n) в функцию.
+      Ваш код определит, является ли переданное число четным (или нет).
+      Функция должна возвращать либо истину, либо ложь.
+      Числа могут быть положительными или отрицательными,
+      целыми числами или числами с плавающей запятой.
+      Поплавки считаются НЕЧЕТНЫМИ для этого ката. 
+*/
+/* 
+function testEven(n) {
+      return n % 2 ? false : true;
+}
+ */
