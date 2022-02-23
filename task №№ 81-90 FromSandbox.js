0da3808d63aa044f2bff053c9bf5ffb533e805d4
Rumata-SL <kirr7619@gmail.com>
2022-02-23 14:14:07 +0500

    Added Task № 81 8 kyu "Can we divide it?"

---
+++ task №№ 81-90 FromSandbox.js
@@ -0,0 +1,16 @@
+"use strict";
+// Task № 81 8 kyu---------------------------
+// Can we divide it?
+/*
+      Your task is to create the functionisDivideBy (or is_divide_by) 
+      to check if an integer number is divisible by both integers a and b. 
+*/
+/*
+      Ваша задача состоит в том, чтобы создать функцию isDivideBy(или ),
+      проверяющую, делится is_divide_byли целое число как на целые числа, так и на .numberab 
+*/
+/* 
+function isDivideBy(number, a, b) {
+      return (number % a === 0) && (number % b === 0)? true : false;
+}
+ */
