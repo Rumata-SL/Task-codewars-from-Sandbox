3e5c7c74f4a7155c3da2e5f6fabf1cfa430dc227
Rumata-SL <kirr7619@gmail.com>
2022-02-23 15:18:14 +0500

    Added Task № 87 8 kyu "Sentence Smash"

--- task №№ 81-90 FromSandbox.js
+++ task №№ 81-90 FromSandbox.js
@@ -116,3 +116,16 @@ class Kata {
       }
 } 
 */
+// Task № 87 8 kyu---------------------------
+// Sentence Smash
+/*
+      Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+*/
+/*
+      Напишите функцию, которая берет массив слов, объединяет их в предложение и возвращает предложение. Вы можете игнорировать необходимость очистки слов или добавления знаков препинания, но вы должны добавлять пробелы между каждым словом. Будьте внимательны, не должно быть пробела ни в начале, ни в конце предложения! 
+*/
+/* 
+function smash(words) {
+      return (words = words.join(" "));
+}
+ */
