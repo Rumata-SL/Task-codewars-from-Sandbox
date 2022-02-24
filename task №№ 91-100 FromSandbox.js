c733eeb5fc518b014dca8eb2f88433511fc5c504
Rumata-SL <kirr7619@gmail.com>
2022-02-24 23:28:54 +0500

    Added Task № 92 8 kyu "Function 2 - squaring an argument"

--- task №№ 91-100 FromSandbox.js
+++ task №№ 91-100 FromSandbox.js
@@ -21,3 +21,17 @@ function feast(beast, dish) {
             : false;
 }
  */
+
+// Task № 92 8 kyu---------------------------
+// Function 2 - squaring an argument
+/*
+      Now you have to write a function that takes an
+      argument and returns the square of it. 
+ */
+/*
+      Теперь вам нужно написать функцию,
+      которая принимает аргумент и возвращает его квадрат. 
+ */
+/* 
+const square = (num) => num * num;
+ */
