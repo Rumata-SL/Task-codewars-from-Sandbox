62da834221d6991d4d6f06f379f0b468e9c0dbcb
Rumata-SL <kirr7619@gmail.com>
2022-02-25 00:43:23 +0500

    Added Task № 95 8 kyu "Total amount of points"

--- task №№ 91-100 FromSandbox.js
+++ task №№ 91-100 FromSandbox.js
@@ -80,3 +80,43 @@ function getMiddle(s) {
             : s.charAt(s.length / 2 - 1).concat(s.charAt(s.length / 2));
 }
  */
+// Task № 95 8 kyu---------------------------
+// Total amount of points
+/*
+      Our football team finished the championship.
+      The result of each match look like "x:y". Results of all matches are
+      recorded in the collection.
+      For example: ["3:1", "2:2", "0:1", ...]
+      Write a function that takes such collection and counts
+      the points of our team in the championship.
+      Rules for counting points for each match:
+      if x>y - 3 points
+      if x<y - 0 point
+      if x=y - 1 point 
+*/
+/*
+      Наша футбольная команда завершила чемпионат.
+      Результат каждого совпадения выглядит как "x:y".
+      Результаты всех матчей фиксируются в сборнике.
+      Например: ["3:1", "2:2", "0:1", ...]
+      Напишите функцию, которая берет такую ​​коллекцию и считает
+      очки нашей команды в чемпионате. Правила подсчета очков за каждый матч:
+      если х>у - 3 балла
+      если х<у - 0 баллов
+      если х=у - 1 балл 
+*/
+/* 
+function points(games) {
+      let count = 0;
+      games.forEach((element) => {
+            if (element[0] > element[element.length - 1]) {
+                  count += 3;
+            } else if (element[0] < element[element.length - 1]) {
+                  count += 0;
+            } else if ((element[0] = element[element.length - 1])) {
+                  count += 1;
+            }
+      });
+      return count;
+}
+ */
