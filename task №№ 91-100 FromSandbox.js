2029512a0e21b2184ef4506757f91869f0023089
Rumata-SL <kirr7619@gmail.com>
2022-02-25 01:55:11 +0500

    Added Task № 97 8 kyu "Count by X"

--- task №№ 91-100 FromSandbox.js
+++ task №№ 91-100 FromSandbox.js
@@ -142,3 +142,28 @@ function points(games) {
       return n <= 1 ? 1 : n * factorial(n - 1);
 }
  */
+// Task № 97 8 kyu---------------------------
+// Count by X
+/*
+      Create a function with two arguments that will return an array of the first (n) multiples of (x).
+      Assume both the given number and the number of times to count will be positive numbers greater than 0.
+      Return the results as an array (or list in Python, Haskell or Elixir).
+      Examples:
+      countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+      countBy(2,5) === [2,4,6,8,10] 
+*/
+/*
+      Создайте функцию с двумя аргументами, которая будет возвращать массив первых (n) кратных (x).
+      Предположим, что и заданное число, и количество подсчетов будут положительными числами больше 0.
+      Верните результаты в виде массива (или списка в Python, Haskell или Elixir).
+      Примеры:
+      countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+      countBy(2,5) === [2,4,6,8,10] 
+*/
+function countBy(x, n) {
+      let z = [];
+      for (let i = 1; i <= n; i++) {
+            z.push(i * x);
+      }
+      return z;
+}
