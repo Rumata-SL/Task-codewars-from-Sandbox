cc5f855aeeccf66809dab15020f9b7895ae4739d
Rumata-SL <kirr7619@gmail.com>
2022-02-25 23:57:43 +0500

    Added Task № 107 8 kyu "5 without numbers !!"

--- task №№ 101 -110 FromSandbox.js
+++ task №№ 101 -110 FromSandbox.js
@@ -123,3 +123,21 @@ function gimme(triplet) {
       }
 }
  */
+// Task № 107 8 kyu---------------------------
+// 5 without numbers !!
+/*
+      Write a function that always returns 5
+      Sounds easy right? Just bear in mind that 
+      you can't use any of the following characters: 0123456789*+-/ 
+*/
+/*
+      Напишите функцию, которая всегда возвращает5
+      Звучит легко, верно? Просто имейте в виду, 
+      что вы не можете использовать ни один из следующих символов:0123456789*+-/ 
+*/
+/* 
+function unusualFive() {
+      let str = "Hello";
+      return str.length;
+}
+ */
