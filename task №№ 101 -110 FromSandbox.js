6a572a879c94b18cdf18521715bfe5a80a745708
Rumata-SL <kirr7619@gmail.com>
2022-02-25 18:26:23 +0500

    Added Task № 102 7 kyu "Remove anchor from URL"

--- task №№ 101 -110 FromSandbox.js
+++ task №№ 101 -110 FromSandbox.js
@@ -15,3 +15,24 @@ function roundToNext5(n) {
       return Math.ceil(n / 5) * 5;
 }
  */
+// Task № 102 7 kyu---------------------------
+// Remove anchor from URL
+/*
+      Complete the function/method so that it returns the
+      url with anything after the anchor (#) removed.
+      Examples
+      "www.codewars.com#about" --> "www.codewars.com"
+      "www.codewars.com?page=1" -->"www.codewars.com?page=1" 
+*/
+/*
+      Завершите функцию/метод, чтобы он возвращал 
+      URL-адрес с чем-либо после #удаления привязки ( ).
+      Примеры
+      "www.codewars.com#about" --> "www.codewars.com"
+      "www.codewars.com?page=1" -->"www.codewars.com?page=1" 
+*/
+/* 
+function removeUrlAnchor(url) {
+      return url.split('#')[0];
+}
+ */
