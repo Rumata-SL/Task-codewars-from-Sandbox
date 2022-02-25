5f8070dbd055ffbe914ddba47001561c74f175df
Rumata-SL <kirr7619@gmail.com>
2022-02-25 18:16:08 +0500

    Added Task № 101 7 kyu "Round up to the next multiple of 5"

---
+++ task №№ 101 -110 FromSandbox.js
@@ -0,0 +1,17 @@
+"use strict";
+// Task № 101 7 kyu---------------------------
+// Round up to the next multiple of 5
+/*
+      Given an integer as input, can you round it
+      to the next (meaning, "higher") multiple of 5? 
+*/
+/*
+      Учитывая целое число в качестве входных данных,
+      можете ли вы округлить его до следующего (то есть «большего») 
+      числа, кратного 5? 
+ */
+/* 
+function roundToNext5(n) {
+      return Math.ceil(n / 5) * 5;
+}
+ */
